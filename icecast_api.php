4d7023a78b3692a3b75145a31fa9eb3bb58cb42d
Okwinza <okwinza@gmail.com>
2013-01-13 17:42:16 +0400

    Initial release

---
+++ icecast_api.php
@@ -0,0 +1,263 @@
+<?
+/*
+/* @package IceCast2 PHP API
+/* http://github.com/okwinza/icecast2-php-api
+/* @author Okwinza
+/* This work is licensed under the Creative Commons Attribution 3.0 Unported License. 
+/* To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/ 
+/* or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA. 
+*/
+if(!defined('IN_APP')){
+	die('Hacking Attempt!');
+}
+
+class IcecastApi {
+
+	private $config = array();
+	private $result = null;
+	private $_memcache = null;
+	private $icecast_xml = null;
+
+	function __construct(array $config){
+		
+		$this->config = $config;
+		$this->icecast_xml = $this->getDataFromIcecast();
+		if(empty($this->icecast_xml)) die('Failed to connect to Icecast Server.');
+		if($this->config['use_memcached']) $this->MemcachedInit();
+
+	}
+	//Preparing response according to its type.
+	// @param (array) $type
+	// @returns xml or json
+	public function Response($type){
+		switch($type){
+			case 'xml':
+				return $this->array_to_xml($this->result);
+			break;
+			case 'json':
+				return $this->array_to_json($this->result);
+			break;		
+		}
+	}
+	
+	public function Request($method,array $args){
+		
+		if(!empty($args['mount']) && !empty($this->config['icecast_mount_fallback_map'][$args['mount']])){
+			$args['mount'] = (!in_array($args['mount'],$this->listMounts(true))) ? $this->config['icecast_mount_fallback_map'][$args['mount']] : $args['mount'];		
+		}
+		
+		$this->result = ($this->config['use_memcached'] === true) ? $this->WithMemcached($method , $args) :  $this->{$method . 'Action'}($args);
+		return $this;
+	}
+	
+	//Memcached wrapper.
+	private function WithMemcached($method, array $args){
+		
+		$key = $method;
+		foreach($args as $k => $val){
+			$key .= '_'.$val;
+		}
+		$data = $this->_memcache->get($key);
+		
+		if(empty($data)){
+			$data = $this->{$method . 'Action'}($args);
+			$this->_memcache->set($key,$data,$this->config['memcached']['compressed'],$this->config['memcached']['lifetime']);
+		}
+		
+		return $data;
+
+	}
+	
+	
+	
+	/* 
+	private function YourCustomMethodAction(array $args){
+	
+		return array('Hello' => 'Im a template for your custom methods.');
+	} 
+	*/
+	
+	
+	/* 	
+	Getting number of listeners for given mountpoint
+	
+	@param array $args
+	@return array 
+	*/
+	
+	private function GetListenersAction(array $args){	
+		extract($args); // $mount
+		
+		$xml = simplexml_load_string($this->icecast_xml);
+		$xml = $xml->xpath("/icestats/source[@mount='/".$mount."']/listeners");
+		
+		$json = json_encode($xml);
+		$array = json_decode($json,TRUE);
+		
+		return array('listeners' => $array[0][0]);
+	}
+	
+	/* 	
+	Getting total number of listeners
+	
+	@param array $args - empty array
+	@return array 
+	*/		
+	private function GetTotalListenersAction(array $args){
+		
+		$xml = simplexml_load_string($this->icecast_xml);
+		$xml = $xml->xpath("/icestats/listeners");
+		
+		$json = json_encode($xml);
+		$array = json_decode($json,TRUE);
+		
+		return array('totalListeners'=>$array[0][0]);
+	}
+	
+	/* 	
+	Getting the current track and its timestamp for given $mount
+	
+	@param array $args[$mount]
+	@return array 
+	*/
+	private function GetTrackAction(array $args){
+		extract($args); // $mount
+		
+		$data = $this->GetHistoryAction(array('mount'=> $mount,'amount'=>1)); //using already defined method.
+		
+		return $data;
+	}	
+	
+	/* 	
+	Parsing the logfile, returning an array of with specified $amount of last tracks for given $mount
+	
+	@param array $args[string $mount, int $amount] 
+	@return array 
+	*/
+	private function GetHistoryAction(array $args){
+		extract($args); // $mount, $amount
+		$amount = ($amount > $this->config['max_amount_of_history']) ? $this->config['max_amount_of_history'] : $amount;
+		
+		$grab_lines = intval($amount * pow(count($this->listMounts()),1.2)); // amount of rows to grab
+		
+		$last_lines = $this->read_backward_line($this->config['playlist_logfile'], $grab_lines);
+		$last_lines = explode("\n",$last_lines);
+		array_pop($last_lines); // deleting last empty line
+		$last_lines = array_reverse($last_lines); // desc. order
+		
+		$line_parsed = array();
+		$result_array = array();
+		$i = 0;
+		foreach($last_lines as $key => $line){
+			$line_parsed = explode("|",$line);
+			if($line_parsed[1] == "/".$mount){
+				if($i < $amount){
+					$result_array[$i]['track'] 	 = $this->filter_string($line_parsed[3]); // getting rid of special chars in the title
+					$result_array[$i]['timestamp'] = strtotime($line_parsed[0]); 
+					$i++;
+				}
+			}
+		}
+		return $result_array;
+	}	
+	
+	private function MemcachedInit(){
+		$this->_memcache = new Memcache();
+		$this->_memcache->pconnect($this->config['memcached']['server'], $this->config['memcached']['port']);	
+	}
+	
+	public function listMounts($only_active = false){
+		
+		$xml = simplexml_load_string($this->icecast_xml);
+		if($only_active){
+			$xml = $xml->xpath("/icestats/source[source_ip != '']/@mount");	
+		}else{
+			$xml = $xml->xpath("/icestats/source/@mount");	
+		}
+		
+		$json = json_encode($xml);
+		$array = json_decode($json,TRUE);
+		
+		$list = array();
+		foreach($array as $key => $item){
+			$list[] = str_replace('/','',$item['@attributes']['mount']);
+		}
+		return $list;
+	}
+
+	
+	
+	private function getDataFromIcecast(){
+		
+		$process = curl_init($this->config['icecast_server_hostname'].':'.$this->config['icecast_server_port'].'/admin/stats');
+
+		curl_setopt($process, CURLOPT_USERPWD, $this->config['icecast_admin_username'] . ":" . $this->config['icecast_admin_password']);
+		curl_setopt($process, CURLOPT_TIMEOUT, 5);
+		curl_setopt($process, CURLOPT_RETURNTRANSFER, TRUE);
+		
+		return curl_exec($process);		
+	}
+	
+	private function array_to_xml($response_array, $xml=null){
+
+        if ($xml == null)
+        {
+            $xml = simplexml_load_string("<?xml version='1.0'?><". $this->config['xmlrootnode'] ."/>");
+        }
+  
+        foreach($response_array as $key => $value)
+        {
+            if (is_numeric($key))
+            {
+				$key = 'item';
+            }
+        
+            $key = preg_replace('/[^a-z]/i', '', $key);
+            if (is_array($value))
+            {
+                $node = $xml->addChild($key);
+                $this->array_to_xml($value, $node);
+            }
+            else
+            {
+                $xml->addChild($key,$value);
+            }
+        }
+        return $xml->asXML();
+    }
+	
+	private function array_to_json($response_array){
+		return json_encode($response_array);
+	}
+	
+	private function read_backward_line($filename, $lines)
+	{
+		$offset = -1;
+		$c = '';
+		$read = '';
+		$i = 0;
+		$fp = @fopen($filename, "r");
+		while( $lines && fseek($fp, $offset, SEEK_END) >= 0 ) {
+			$c = fgetc($fp);
+			if($c == "\n" || $c == "\r"){
+				$lines--;
+			}
+			$read .= $c;
+			$offset--;
+		}
+		fclose ($fp);
+		return strrev(rtrim($read,"\n\r"));
+	}
+	
+	private function filter_string($string){
+		$search = array("'","&","@","\"");
+		return str_replace($search,"",$string);
+	}
+	
+	
+	
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,128 @@
+<?php
+/*
+/* @package IceCast2 PHP API
+/* http://github.com/okwinza/icecast2-php-api
+/* @author Okwinza
+/* This work is licensed under the Creative Commons Attribution 3.0 Unported License. 
+/* To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/ 
+/* or send a letter to Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA. 
+*/
+
+define('IN_APP', true);
+require 'vendor/autoload.php';
+require 'icecast_api.php';
+
+//IceCast API Config
+$config = array(
+	'icecast_server_hostname' 		 => 'radio.example.com', //icecast2 server hostname or IP
+	'icecast_server_port'			 => 80, 
+	'icecast_admin_username' 		 => 'admin', //admin username
+	'icecast_admin_password' 		 => 'password', //admin password
+	//If you have an event based mounts(e.g. for live broadcasting), 
+	//you should configure fallback map below according to your icecast2 config file.
+	//Read the docs for more info.
+	'icecast_mount_fallback_map' => array('live' => 'nonstop',  // from => to
+									  'trance.live'  => 'trance.nonstop',
+									  'house.live'	=> 'house.nonstop'),
+	'playlist_logfile' 		 => '/var/log/icecast2/playlist.log', // must be available for reading
+	'use_memcached' 		 => true, // using of the memcached: true | false
+	'memcached' 			 => array('server' 		=> '127.0.0.1', 
+									  'port' 		=> 11211, 
+									  'lifetime' 	=> 10, // lifetime of the cache in seconds
+									  'compressed'  => 0), // compress data stored with memcached? 1 or 0. Requires zlib.
+	'max_amount_of_history'	 => '20', // max limit of requested items of playback history
+	'xmlrootnode'			 => 'response' // Root node name for the response using XML.
+);
+
+//initiate Slim
+$app = new \Slim\Slim();
+//initiate IcecastApi model
+$icecastApi = new icecastApi($config);
+
+
+//No active mounts -- nothing to do here, shutting down...
+$active_mounts = $icecastApi->listMounts(true);
+if(empty($active_mounts)){
+    // build response
+    $response = array(
+        'type' => '503',
+        'message' => 'Unavailable'
+    );
+
+    // output response and exit.
+    $app->halt(503, json_encode($response));
+}
+
+
+//Number of listeners of specified mountpoint.
+//(string) :mount 		 => one of the existing mounts
+//(string) :responseType => response type(json|xml)
+$app->get('/listeners/:mount/:responseType(/)', function ($mount,$responseType) use ($icecastApi, $app) {
+	
+	$app->response()->header("Content-Type", "application/".$responseType);	//setting appropriate headers
+	echo $icecastApi->Request('GetListeners',array('mount' => $mount))->Response($responseType); //returning response to the client
+	
+})->conditions(array("mount" => "(". implode('|',$icecastApi->listMounts()) .")", "responseType" => "(json|xml)")); // Only existing mounts are allowed
+
+
+//Current track of specified mountpoint.
+//(string) :mount 		 => one of the existing mounts
+//(string) :responseType => response type(json|xml)
+$app->get('/track/:mount/:responseType(/)', function ($mount,$responseType) use ($icecastApi, $app) {
+
+	$app->response()->header("Content-Type", "application/".$responseType); //setting appropriate headers
+	echo $icecastApi->Request('GetTrack',array('mount' => $mount))->Response($responseType); //returning response to the client
+	
+})->conditions(array("mount" => "(". implode('|',$icecastApi->listMounts()) .")", "responseType" => "(json|xml)"));
+
+
+//Last tracks of specified mountpoint.
+//(string) :mount 	     => one of the existing mounts
+//(int)    :amount 		 => amount of tracks to retrieve
+//(string) :responseType => response type(json|xml)
+$app->get('/history/:mount/:amount/:responseType(/)', function ($mount,$amount,$responseType) use ($icecastApi, $app) {
+
+	$app->response()->header("Content-Type", "application/".$responseType); //setting appropriate headers
+	echo $icecastApi->Request('GetHistory',array('mount' => $mount , 'amount' => $amount))->Response($responseType); //returning response to the client
+	
+})->conditions(array("mount" => "(". implode('|',$icecastApi->listMounts()) .")", "amount" => "\d+" , "responseType" => "(json|xml)"));
+
+
+//Total listeners
+//(string) :responseType => response type(json|xml)
+$app->get('/totalListeners/:responseType(/)', function ($responseType) use ($icecastApi, $app) {
+
+	$app->response()->header("Content-Type", "application/".$responseType); //setting appropriate headers
+	echo $icecastApi->Request('GetTotalListeners',array())->Response($responseType); //returning response to the client
+	
+})->conditions(array("responseType" => "(json|xml)"));
+
+
+
+
+//Custom method template
+/* 
+$app->get('/customMethod/:responseType(/)', function ($responseType) use ($icecastApi, $app) {
+
+	$app->response()->header("Content-Type", "application/".$responseType);
+	echo $icecastApi->Request('YourCustomMethod',array())->Response($responseType);
+	
+})->conditions(array("responseType" => "(json|xml)")); 
+*/
+
+
+$app->notFound(function () use ($app) {
+
+    // build response
+    $response = array(
+        'type' => '400',
+        'message' => 'Bad request'
+    );
+
+    // output response and exit
+    $app->halt(400, json_encode($response));
+});
+
+$app->run();
+
+?>
